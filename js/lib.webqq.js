3908523dbccbaa9b3e53ee27b248a1f45a5e645c
Xiang Gao <phoenix.x.gao@gmail.com>
2013-07-12 09:20:12 +0800

    new webqq api

---
+++ js/lib.webqq.js
@@ -0,0 +1,29 @@
+var WebQQ = {
+    /**
+     * 登录api
+     * @param u =QQ号码 
+     * @param p =encoded password
+     */
+    URI_LOGIN_SSL : 'https://ssl.ptlogin2.qq.com/login', 
+    
+    /**
+     * QQ帐号 
+     */
+    account: null,
+    
+    /**
+     * 在线状态 
+     */
+    status: null,
+    
+    doLogin: function() {
+        
+    },
+    
+    /**
+     * 记录登录日志
+     */
+    _logLogin: function() {
+        
+    }
+};
